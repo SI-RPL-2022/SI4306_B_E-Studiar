1b62fd288550c8a7819a9134ed8f02e01220b855
Alpha-Iso <hafizalarsyad74@gmail.com>
2022-04-30 11:48:16 +0700

    [Done: add register login pelajar resolve #5]

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,48 +1,33 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Masuk Estudiar</title>
-</head>
-
-<body>
-    <div class="row justify-content-center" style="margin-top: 150px">
-        <div class="col-lg-6">
-            <main class="form-registration">
-                <h1 class="h3 mb-3 fw-normal text-center">Masuk Estudiar</h1>
-                <form action="/user/login" method="POST">
-                    @csrf
-                    <div class="form-floating">
-                        <input type="email" class="form-control " name="email" id="email" required
-                            value="{{ old('email') }}" placeholder="name@example.com">
-                        <label for="email">Alamat Email</label>
+@extends('layout')
+@section('content')
+<section class="min-vh-100 h-custom primary-bg">
+    <div class="container py-5 h-100 d-flex justify-content-center align-items-center">
+        <div class="col-10 card">
+            <div class="row">
+                <div class="p-5">
+                    <div class="mb-4 text-center">
+                        <h1>Masuk Estudiar </h1>
                     </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control rounded-bottom" name="password" id="password" required
-                            placeholder="Password">
-                        <label for="password">Password</label>
-                    </div>
+                    <form role="form" action="/user/login" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label class="form-label">Email address</label>
+                            <input type="email" name="email" placeholder="Masukan alamat email" class="form-control"
+                                required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
 
-                    <button class="w-100 btn btn-lg mt-3" style="background-color: #1780E2; color:white;" type="submit">Login</button>
-                </form>
-                <small class="d-block mt-3">Belum memiliki akun? <a class="text-primary" href="/user/register">
-                        Daftar
-                        sekarang!</a></small>
-            </main>
+                        <button type="submit" class="btn col-12 btn-primary">Masuk</button>
+                    </form>
+                    <small class="d-block mt-3">Belum memiliki akun? <a class="text-primary" href="/user/register">
+                            Daftar
+                            sekarang!</a></small>
+                </div>
+            </div>
+
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+    @endsection
